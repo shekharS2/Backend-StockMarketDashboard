4b372dbfc8346352e7fe8f35d9c74d4e752441af
Shekhar <shekhar@Shekhars-Laptop.local>
2025-01-06 09:51:48 +0530

    Initial commit

---
+++ src/main/java/com/example/StockMarketDashboard/StockMarketDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.StockMarketDashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMarketDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockMarketDashboardApplication.class, args);
+	}
+
+}
